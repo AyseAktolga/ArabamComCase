8a864ca38ec9c89780c411dd8343b0e39bd671f6
Ayşe Aktolga <aaysesari@yahoo.com>
2023-09-14 23:52:55 +0300

    added repositories and entities

---
+++ ArabamComCase.Application/Repositories/IAdvertVisitRepository.cs
@@ -0,0 +1,13 @@
+﻿using ArabamComCase.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabamComCase.Application.Repositories
+{
+    public interface IAdvertVisitRepository : IRepository<AdvertVisit>
+    {
+    }
+}
---
+++ ArabamComCase.Core/Entities/AdvertVisit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabamComCase.Core.Entities
+{
+    public class AdvertVisit
+    {
+        public int AdvertId { get; set; }
+
+        public string? IpAdress { get; set; }
+
+        public DateTime VisitDate { get; set; }
+    }
+}
